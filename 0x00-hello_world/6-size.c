09aca3511067fd206a55aa40d0dc932a8ff2e696
Your Name <you@example.com>
2022-07-01 04:28:08 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,47 +1,49 @@
-
+#include <stdio.h>
 
 
 
 /**
  *
- *  * main - Entry point
+ *  *  main - Enty point
  *
  *   *
  *
- *    * Return: Always 0 (Success)
+ *    *  Return: Always 0 (Success)
  *
  *     */
+
+
 
 int main(void)
 
 {
 
-		int a;
+		char acharacter;
 
-			long int b;
+			int ainteger;
 
-				long long int c;
+				long along;
 
-					char d;
+					long long alonglong;
 
-						float f;
+						float afloat;
 
 
 
-							printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+							printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
 
-								printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+								printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
 
-									printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+									printf("Size of a long int: %lu byte(s)\n", sizeof(along));
 
-										printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+										printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
 
-											printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+											printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
+
+
 
 												return (0);
 
+
+
 }
-
-Footer
-
-© 2022 GitHub, Inc.
