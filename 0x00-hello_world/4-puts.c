d963f270be913abd8d9472041a9fd01d36487862
Your Name <you@example.com>
2022-07-01 03:53:48 -0700

    c hello world is completed

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - Write '"Programming is like building a multilingual puzzle'
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - Prints "with proper grammar, but the ouptu is a piece of art,"
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
