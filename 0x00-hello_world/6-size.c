622f06d091f98a96a62821d18f06aa621c3fc0fb
Your Name <you@example.com>
2022-07-01 04:39:12 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,49 +1,25 @@
-#include <stdio.h>
-
-
-
-/**
- *
- *  *  main - Enty point
- *
- *   *
- *
- *    *  Return: Always 0 (Success)
- *
- *     */
-
-
-
-int main(void)
-
-{
-
-		char acharacter;
-
-			int ainteger;
-
-				long along;
-
-					long long alonglong;
-
-						float afloat;
-
-
-
-							printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
-
-								printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
-
-									printf("Size of a long int: %lu byte(s)\n", sizeof(along));
-
-										printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
-
-											printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
-
-
-
-												return (0);
-
-
-
-}
+#include <stdio.h>                                                                                                                 
+                                                                                                                                   
+/**                                                                                                                                
+ * main - Enty point                                                                                                               
+ *                                                                                                                                 
+ * Return: Always 0 (Success)                                                                                                      
+ */                                                                                                                                
+                                                                                                                                   
+  int main(void)                                                                                                                   
+  {                                                                                                                                
+                char acharacter;                                                                                                   
+                int ainteger;                                                                                                      
+                long along;                                                                                                        
+                long long alonglong;                                                                                               
+                float afloat;                                                                                                      
+                                                                                                                                   
+                printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));                                                       
+                printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));                                                         
+                printf("Size of a long int: %lu byte(s)\n", sizeof(along));                                                        
+                printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));                                               
+                printf("Size of a float: %lu byte(s)\n", sizeof(afloat));                                                          
+                                                                                                                                   
+                return (0);                                                                                                        
+                                                                                                                                   
+}                               
