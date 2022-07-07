492c13cac794f78e572a83ed148e7110912e51cd
Your Name <you@example.com>
2022-07-07 01:16:08 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - checkes upper case
+ * @c: print int
+ * Return: give 1 or 0
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
