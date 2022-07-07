7482f41c95e8efcacceca6b3dccfda8e179ac8c5
Your Name <you@example.com>
2022-07-07 12:36:02 -0700

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,36 +1,41 @@
 #include "main.h"
+	
 
-/**
- * print_triangle - check for a digit
- * @size : integer type
- * Return:void
- */
+	/**
+	 * print_triangle - check for a digit
+	 * @size : integer type
+	 * Return:void
+	 */
+	
 
-void print_triangle(int size)
-{
+	void print_triangle(int size)
+	{
+	
 
-        int i = 1, ii;
+		int i = 1, ii;
+	
 
-        while (i <= size && size > 0)
-        {
-                ii = 0;
-                while (ii < size - i)
-                {
-                        _putchar(' ');
-                        ii++;
-                }
-                ii = 0;
-                while (ii < i)
-                {
-                        _putchar('#');
-                        ii++;
-                }
+		while (i <= size && size > 0)
+		{
+			ii = 0;
+			while (ii < size - i)
+			{
+				_putchar(' ');
+				ii++;
+			}
+			ii = 0;
+			while (ii < i)
+			{
+				_putchar('#');
+				ii++;
+			}
+	
 
-                _putchar('\n');
-                i++;
-        }
-        if (i == 1)
-                _putchar('\n');
-}
+			_putchar('\n');
+			i++;
+		}
+		if (i == 1)
+			_putchar('\n');
+	}
 
 
