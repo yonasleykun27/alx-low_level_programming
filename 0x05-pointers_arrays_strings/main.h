c0e0524816a5542aa6633e4034e62374fb0ab837
Your Name <you@example.com>
2022-07-11 00:03:11 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+void reset_to_98(int *n);
+	void swap_int(int *a, int *b);
+	int _strlen(char *s);
+	void _puts(char *str);
+	int _putchar(char c);
+	void print_rev(char *s);
+	void puts2(char *str);
+	void puts_half(char *str);
+	void print_array(int *a, int n);
+	char *_strcpy(char *dest, char *src);
+	void rev_string(char *s);
+
+
