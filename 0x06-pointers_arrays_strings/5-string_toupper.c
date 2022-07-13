2c712a4f16fb0d6b9b74ebaf6a4f1c7c44120d75
Your Name <you@example.com>
2022-07-12 23:08:54 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *rev_string - reverse the string.
+ *@s: pointer to string.
+ *
+ *Return: void.
+ */
+void rev_string(char *s)
+{
+	int i, j, k, temp;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	k = 0;
+	j = i - 1;
+
+	while (k < j)
+	{
+		temp = s[k];
+		s[k] = s[j];
+		s[j] = temp;
+		k++;
+		j--;
+	}
+}
