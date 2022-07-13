851e1598e6b0f1a3eee3893b385f56f63ba21b3d
Your Name <you@example.com>
2022-07-12 23:02:40 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Function that returns the length of a string
+ *
+ * @s: this is the input string
+ *
+ * Return: Lenngth of the string
+ */
+int _strlen(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0' ; index++)
+		;
+	return (index);
+}
