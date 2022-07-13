4f18bced6d807ad1d6ee7d916fbed52fabef2894
Your Name <you@example.com>
2022-07-12 23:00:35 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swaps the values of two integers.
+ * @a: this is the first entry
+ * @b: this is the second entry
+ *
+ *
+ */
+void swap_int(int *a, int *b)
+{
+	int changed;
+
+	changed = *a;
+	*a = *b;
+	*b = changed;
+}
