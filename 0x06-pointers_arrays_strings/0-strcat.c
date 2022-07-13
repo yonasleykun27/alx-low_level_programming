c57e339c04201d6c5036d6af45b0032de26f4504
Your Name <you@example.com>
2022-07-12 22:58:22 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to an int as parameter and
+ * updates the value it points to to 98.
+ *
+ * @n: This is the entry
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
