1cc99b68f5728bb6e86cf145e3abf6af590ce736
Your Name <you@example.com>
2022-07-14 02:25:37 -0700

    task 09

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,26 +1,22 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
-  * print_number - print numbers chars
-  * @n: integer params
-  * Return: 0
- **/
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
+ */
 
 void print_number(int n)
 {
-unsigned int n1;
+	unsigned int num = n;
 
-n1 = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
 
-if (n < 0)
-{
-_putchar('-');
-n1 = -n;
-}
+	if ((num / 10) > 0)
+		print_number(num / 10);
 
-if (n1 / 10 != 0)
-{
-print_number(n1 / 10);
-}
-_putchar((n1 % 10) + '0');
+	_putchar((num % 10) + '0');
 }
