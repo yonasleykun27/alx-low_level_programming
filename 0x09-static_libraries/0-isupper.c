66a6932d92bb6c70f1beea939061361fada6affb
Your Name <you@example.com>
2022-07-22 09:37:02 -0700

    finished

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,14 +1,22 @@
 #include "main.h"
-
 /**
- * _isupper - checks for uppercase character
- * @c: character that is checked
- * Return: if uppercase returns 0 otherwise
+ * _isupper - checkes upper case
+ * @c: print int
+ * Return: give 1 or 0
  */
 
 int _isupper(int c)
 {
 if (c >= 'A' && c <= 'Z')
+{
 return (1);
+}
+
+else
+{
 return (0);
 }
+
+}
+
+
