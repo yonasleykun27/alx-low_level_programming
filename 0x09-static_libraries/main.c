0cd96fcf8f2c8f2f1dc0c4b0c6b024c3a7bceb9d
Your Name <you@example.com>
2022-07-22 08:19:38 -0700

    task

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int main(void)
+{
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return (0);
+}
+julien@ubuntu:~/0x09. Static Librairies$ gcc -std=gnu89 main.c -L. -lmy -o quote
+julien@ubuntu:~/0x09. Static Librairies$ ./quote 
+"At the end of the day, my goal was to be the best hacker"
+    - Kevin Mitnick
