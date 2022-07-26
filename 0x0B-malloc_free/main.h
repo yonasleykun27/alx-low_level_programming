72b832aec3c779df1d675d9010a8fa978db32633
Your Name <you@example.com>
2022-07-26 07:34:16 -0700

    task 0

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,7 @@
+void free_grid(int **grid, int height);
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+int **alloc_grid(int width, int height);
+char *argstostr(int ac, char **av);  
