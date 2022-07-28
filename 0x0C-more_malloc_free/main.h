1125d161601fd7c7cce17579d7412b2a3a335c7e
Your Name <you@example.com>
2022-07-28 05:15:38 -0700

    task 5

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,15 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void _puts(char *str);
+#include <stdio.h>
+#include <stdlib.h>
+
+#define min(x, y) (((x) < (y)) ? (x) : (y))
+
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+int len(char *str);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int _putchar(char c);
-int _atoi(const char *s);
-void print_int(unsigned long int n);
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
 
-
-#endif
+#endif /*MAIN_H*/
