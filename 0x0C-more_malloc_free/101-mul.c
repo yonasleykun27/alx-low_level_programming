df6bea1825c738b611c3dcb18625ea4a295d3bca
Your Name <you@example.com>
2022-07-28 07:56:34 -0700

    task 0

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -222,5 +222,25 @@ int main(int argc, char *argv[])
 	putchar('\n');
 	free(next_prod);
 	free(final_prod);
-	return (0);
+int main(int argc, char *argv[])
+{
+unsigned long mul;
+int i, j;
+	if (argc != 3)
+	{ printf("Error\n");
+	exit(98); }
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{  printf("Error\n");
+			exit(98); }
+		}
+
+	}
+	mul = atol(argv[1]) *  atol(argv[2]);
+	printf("%lu\n", mul);
+return (0);
 }
+
