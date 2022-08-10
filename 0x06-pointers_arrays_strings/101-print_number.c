ece80f2e01561c2b2e241719e1c103108f389f9c
Yonas Leykun <yonasleykun27@gmail.com>
2022-08-10 13:17:23 -0700

    task 10

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,16 +9,17 @@ void print_number(int n)
 {
 		unsigned int n1;
 
-if (n < 0)
+		if (n < 0)
 		{
-					n1 = -n;
-							_putchar('-');
-								} else
-										{
-													n1 = n;
-														}
+			n1 = -n;
+			_putchar('-');
+		} else
+		{
+			n1 = n;
+		}
 
-				if (n1 / 10)
-							print_number(n1 / 10);
-				_putchar((n1 % 10) + '0');
+		if (n1 / 10)
+			print_number(n1 / 10);
+
+		_putchar((n1 % 10) + '0');
 }
