18819ac17b322828e344343639dcae1052b2123f
Yonas Leykun <yonasleykun27@gmail.com>
2022-08-18 14:28:45 -0700

    task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,22 +8,22 @@
  *      */
 unsigned int binary_to_uint(const char *b)
 {
-		int i;
-			unsigned int num;
+	int i;
+	unsigned int num;
 
-				num = 0;
-					if (!b)
-								return (0);
-						for (i = 0; b[i] != '\0'; i++)
-								{
-											if (b[i] != '0' && b[i] != '1')
-															return (0);
-												}
-							for (i = 0; b[i] != '\0'; i++)
-									{
-												num <<= 1;
-														if (b[i] == '1')
-																		num += 1;
-															}
-								return (num);
-}
+	num = 0;
+	if (!b)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+			if (b[i] != '0' && b[i] != '1')
+				return (0);
+		}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+			num <<= 1;
+			if (b[i] == '1')
+				num += 1;
+		}
+	return (num);
+}}
